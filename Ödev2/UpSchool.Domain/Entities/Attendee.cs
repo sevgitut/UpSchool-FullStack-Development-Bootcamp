422d9cc945b14512d15d6465292d845acbc51b8e
Sevgi Tut <sevgitut.07@gmail.com>
2023-03-07 20:39:20 +0300

    HW2

---
+++ Ödev2/UpSchool.Domain/Entities/Attendee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpSchool.Domain.Common;
+
+namespace UpSchool.Domain.Entities
+{
+    public class Attendee : EntityBase
+    {
+        public string FullName { get; set; }
+    }
+}
---
+++ Ödev2/UpSchool.Wasm/Services/ToasterService.cs
@@ -0,0 +1,27 @@
+﻿using Blazored.Toast.Configuration;
+using Microsoft.JSInterop;
+using Shipwreck.BlazorJqueryToast;
+using UpSchool.Domain.Services;
+
+namespace UpSchool.Wasm.Services
+{
+    public class ToasterService : IToasterService
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public ToasterService(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public void ShowSuccess(string message)
+        {
+            _jsRuntime.ShowToastAsync(new ToastOptions
+            {
+                Text = message,
+                Position = Shipwreck.BlazorJqueryToast.ToastPosition.TopCenter,
+                Heading = "UpSchool"
+            });
+        }
+    }
+}
