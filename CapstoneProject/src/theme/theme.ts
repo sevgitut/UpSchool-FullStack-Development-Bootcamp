4e9990d02c1c5baca56829a2ceca0846b5980c2e
Sevgi Tut <sevgitut.07@gmail.com>
2023-08-01 22:08:36 +0300

    commit

---
+++ CapstoneProject/src/theme/theme.ts
@@ -0,0 +1,21 @@
+import { Theme } from '@mui/material/styles';
+
+export const theme: Theme = {
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#ff9a76',
+        },
+        secondary: {
+            main: '#B97A95',
+        },
+        background: {
+            default: '#fff5ee',
+        },
+    },
+    typography: {
+        fontFamily: 'sans-serif',
+        fontWeightRegular: 400,
+        fontWeightMedium: 600,
+    },
+};
